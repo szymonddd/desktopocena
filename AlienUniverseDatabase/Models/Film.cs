81add1dc53b1f68243fba052d4360cc4f97497ca
Turas <182771626+Turas39@users.noreply.github.com>
2025-12-10 18:09:28 +0100

    dodanie modeli

--- AlienUniverseDatabase/Models/Film.cs
+++ AlienUniverseDatabase/Models/Film.cs
@@ -4,16 +4,15 @@ namespace AlienUniverseDatabase.Models;
 
 public class Film
 {
-    public string TytulOryginalny { get; set; }
-    public string TytulPolski { get; set; }
-    public int RokPremiery { get; set; }
-    public string Rezyser { get; set; }
-    public string Scenariusz { get; set; }
-    public string Gatunek { get; set; }
-    public string CzasTrwania { get; set; }
-    public double Ocena { get; set; }
-    public string Statek { get; set; }
-    public string OpisFabuly { get; set; }
-    public string Ciekawostka { get; set; }
-    public List<Postac> Postacie { get; set; } = new List<Postac>();
+    public string TytulOryginalny { get; set; } = "";
+    public string TytulPolski { get; set; } = "";
+    public int? RokPremiery { get; set; } 
+    public string Rezyser { get; set; } = "";
+    public string Gatunek { get; set; } = "";
+    public int? CzasTrwania { get; set; } 
+    public double? Ocena { get; set; }
+    public List<string> Postacie { get; set; } = new List<string>();
+    public string Statek { get; set; } = "";
+    public string OpisFabuly { get; set; } = "";
+    public string Ciekawostka { get; set; } = "";
 }
--- AlienUniverseDatabase/Models/Postac.cs
+++ AlienUniverseDatabase/Models/Postac.cs
@@ -2,12 +2,11 @@ namespace AlienUniverseDatabase.Models;
 
 public class Postac
 {
-    public string ImieNazwisko { get; set; }
-    public string Rola { get; set; }
-    public string Aktor { get; set; }
-    public string Rasa { get; set; }
-    public int RokUrodzenia { get; set; }
-    public string Charakterystyka { get; set; }
-    public string Los { get; set; }
-    public string Ciekawostka { get; set; }
+    public string ImieNazwisko { get; set; } = "";
+    public string Film { get; set; } = "";
+    public string Rola { get; set; } = "";
+    public string Aktor { get; set; } = "";
+    public string Rasa { get; set; } = "";
+    public int? RokUrodzenia { get; set; }
+
 }
