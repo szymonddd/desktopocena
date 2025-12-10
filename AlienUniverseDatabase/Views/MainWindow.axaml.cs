96358be5839b83a5d87ed2c4fa69918866f86cc2
Szymon D <szymon.dabrowski@cyfroweszkoly.pl>
2025-12-10 22:26:43 +0100

    main window commit

--- AlienUniverseDatabase/Views/MainWindow.axaml.cs
+++ AlienUniverseDatabase/Views/MainWindow.axaml.cs
@@ -1,15 +1,32 @@
-using System.Collections.Generic;
+using System;
+using System.Reactive;
+using System.Reactive.Disposables;
+using AlienUniverseDatabase.ViewModels;
 using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
 
 namespace AlienUniverseDatabase.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
         InitializeComponent();
+        
+        this.WhenActivated((CompositeDisposable disposables) =>
+        {
+            ViewModel!.ShowSummaryWindow.RegisterHandler(async interaction =>
+            {
+                var win = new SummaryWindow()
+                {
+                    DataContext = new SummaryWindowViewModel(interaction.Input)
+                };
+                
+                
+                await win.ShowDialog(this);
+                interaction.SetOutput(Unit.Default);
+            }).DisposeWith(disposables);
+        });
     }
-
-
-    
 }
