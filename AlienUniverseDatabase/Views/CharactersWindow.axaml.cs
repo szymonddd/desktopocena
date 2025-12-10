dc59208fe65362fa8789efac2aacc041f2cad7c5
Szymon D <szymon.dabrowski@cyfroweszkoly.pl>
2025-12-10 22:11:40 +0100

    views commit

---
+++ AlienUniverseDatabase/Views/CharactersWindow.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using AlienUniverseDatabase.ViewModels;
+
+namespace AlienUniverseDatabase.Views;
+
+public partial class CharactersWindow : Window
+{
+    public CharactersWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ AlienUniverseDatabase/Views/SummaryWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AlienUniverseDatabase.Views;
+
+public partial class SummaryWindow : Window
+{
+    public SummaryWindow()
+    {
+        InitializeComponent();
+    }
+}
